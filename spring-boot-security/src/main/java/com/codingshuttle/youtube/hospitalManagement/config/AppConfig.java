cb3160789e4f62881f3e2fe004081b9543399be1
toriqulislam <toriqul@mymedicalhub.com>
2025-10-24 19:05:01 +0600

    Introduce Spring Security with User Management:
    - Added `User` entity implementing `UserDetails` for authentication.
    - Configured `WebSecurityConfig` for role-based access, form login, and in-memory user details.
    - Integrated `BCryptPasswordEncoder` for password encoding in `AppConfig`.
    - Updated `application.properties` to remove sensitive information and adjust database configurations.

--- spring-boot-security/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
+++ spring-boot-security/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
@@ -3,6 +3,15 @@ package com.codingshuttle.youtube.hospitalManagement.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +19,19 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+//    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("admin").password(passwordEncoder().encode("pass")).roles("ADMIN").build();
+        UserDetails userDetails2 = User.withUsername("patient").password(passwordEncoder().encode("pass")).roles("PATIENT").build();
+
+        return new InMemoryUserDetailsManager(userDetails, userDetails2);
     }
 }
---
+++ spring-boot-security/src/main/java/com/codingshuttle/youtube/hospitalManagement/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.codingshuttle.youtube.hospitalManagement.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/public/**").permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
+                        )
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("admin").password(passwordEncoder.encode("pass")).roles("ADMIN").build();
+        UserDetails userDetails2 = User.withUsername("patient").password(passwordEncoder.encode("pass")).roles("PATIENT").build();
+
+        return new InMemoryUserDetailsManager(userDetails, userDetails2);
+    }
+}
